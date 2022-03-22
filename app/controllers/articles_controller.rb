c031856f0571070021fb90cbf385efe296042baa
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-03-22 09:05:24 +0545

    feat: create and read operation complete

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,29 @@
+class ArticlesController < ApplicationController
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find_by_id(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    if @article.save
+      @article.save
+      redirect_to article_path(@article)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+end
