2819b90d21299058e6af0db577e7748217f6c767
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-03-27 09:31:51 +0545

    feat: user authentication model setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_many :articles
   validates :username, presence: true, uniqueness: true
   validates :email, format: { with: /\A([a-zA-Z0-9_\-.]+)@([a-zA-Z0-9_\-.]+)\.([a-zA-Z]{2,5})\z/ }
+  has_secure_password
+  validates :password, presence: true
 end
