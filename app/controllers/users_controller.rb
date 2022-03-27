76d0e06c9ea6bc6fcf8693cff2ae3fb27221de0a
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-03-27 17:53:02 +0545

    feat: user show and index action complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,9 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i[show edit update destroy]
+  def index
+    @users = User.all
+  end
+
   def new
     @user = User.new
   end
