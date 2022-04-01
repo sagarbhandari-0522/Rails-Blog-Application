8f2b54f807fad7fae60de395c8e355d2b991d9ff
sagarbhandari-0522 <sagarvandari1@gmail.com>
2022-04-01 14:41:46 +0545

    feat:add category selection on creating article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,6 +18,7 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.user = current_user
+
     if @article.save
       flash[:success] = 'Articles Created Successfully'
       redirect_to article_path(@article)
@@ -56,7 +57,7 @@ class ArticlesController < ApplicationController
   private
 
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def find_article
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ApplicationRecord
   has_many :article_categories
   has_many :articles, through: :article_categories
-  validates :name, presence: true, length: { in: 6..20 }, uniqueness: true
+  validates :name, presence: true, length: { in: 3..20 }, uniqueness: true
 end
